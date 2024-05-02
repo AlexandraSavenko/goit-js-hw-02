1ea9f9d4ec6b3dba2a9ab13d71d1f1e48b5f9aaa
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-05-02 15:27:34 +0300

    task three done

--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,18 @@
-function formatMessage(message, maxLength) {
-  //   let theLeng = maxLength.length;
-  //   console.log(theLeng);
-  let messageLength = message.length;
-  console.log(messageLength);
-  if (maxLength === messageLength) {
-    return `"${message}"`;
-  } else {
-    let partMessage = message.slice(0, maxLength);
-    return `"${partMessage}..."`;
-  }
-}
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+// function formatMessage(message, maxLength) {
+//   //   let theLeng = maxLength.length;
+//   //   console.log(theLeng);
+//   let messageLength = message.length;
+//   console.log(messageLength);
+//   if (maxLength === messageLength) {
+//     return `"${message}"`;
+//   } else {
+//     let partMessage = message.slice(0, maxLength);
+//     return `"${partMessage}..."`;
+//   }
+// }
+// console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+// console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+// console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+// console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
